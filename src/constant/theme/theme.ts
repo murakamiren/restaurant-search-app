0ab099d3e32e2e77818f7420197b6ae62d530da0
renmurakami <autoeyy@gmail.com>
2022-11-05 23:11:20 +0900

    feat: floating label

--- src/constant/theme/theme.ts
+++ src/constant/theme/theme.ts
@@ -1,9 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
 
+import floatingLabelTheme from "./floatingLabelTheme";
 import fontsTheme from "./fontsTheme";
 
 const theme = extendTheme({
   fontsTheme,
+  floatingLabelTheme,
 });
 
 export default theme;
