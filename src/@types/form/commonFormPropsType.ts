29408bca71f2ff6a6c8006dd8a3f8fc9f11678dd
renmurakami <autoeyy@gmail.com>
2022-11-06 00:37:54 +0900

    feat: add type

--- src/@types/form/commonFormPropsType.ts
+++ src/@types/form/commonFormPropsType.ts
@@ -1,9 +1,11 @@
-import { UseFormRegister } from "react-hook-form";
+import { Path, UseFormRegister } from "react-hook-form";
 
 import SearchFormValueType from "./searchFormValueType";
 
 type CommonFormProps = {
   register: UseFormRegister<SearchFormValueType>;
+  registerName: Path<SearchFormValueType>;
+  labelText: string;
 };
 
 export default CommonFormProps;
