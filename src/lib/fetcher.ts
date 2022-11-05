3c220c9620f0c0717229e7f726b0cf26f1cabe7a
renmurakami <autoeyy@gmail.com>
2022-11-05 11:26:34 +0900

    refactor: jsDoc

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * fetcher to use swr
+ * @param url axios.get(url)
+ * @returns return res.data
+ */
 const fetcher = async (url: string) => {
   const res = await axios.get(url);
   const data = await res.data;
