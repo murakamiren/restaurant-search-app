02a844e64dff0be3b842179c8cb5da333ecca8b0
renmurakami <autoeyy@gmail.com>
2022-11-06 10:07:12 +0900

    fix: theme color

--- src/constant/theme/color.ts
+++ src/constant/theme/color.ts
@@ -1,20 +1,14 @@
-import { extendTheme } from "@chakra-ui/react";
-
-const customColor = extendTheme({
-  colors: {
-    main: {
-      "50": "#FDF6E7",
-      "100": "#FAE5BC",
-      "200": "#F7D491",
-      "300": "#F4C366",
-      "400": "#F1B13B",
-      "500": "#EFA010",
-      "600": "#BF800D",
-      "700": "#8F600A",
-      "800": "#5F4007",
-      "900": "#302003",
-    },
+export const primary = {
+  primary: {
+    50: "#FDF6E7",
+    100: "#FAE5BC",
+    200: "#F7D491",
+    300: "#F4C366",
+    400: "#F1B13B",
+    500: "#EFA010",
+    600: "#BF800D",
+    700: "#8F600A",
+    800: "#5F4007",
+    900: "#302003",
   },
-});
-
-export default customColor;
+};
--- src/constant/theme/theme.ts
+++ src/constant/theme/theme.ts
@@ -1,15 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 
-import customColor from "./color";
+import { primary } from "./color";
 import floatingLabelTheme from "./floatingLabelTheme";
 import fontsTheme from "./fontsTheme";
 
 const theme = extendTheme({
   fontsTheme,
+  colors: primary,
   components: {
     Form: floatingLabelTheme,
   },
-  customColor,
 });
 
 export default theme;
