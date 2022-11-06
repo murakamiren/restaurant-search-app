41f60e3660510153138e8d2caf15b78cfa9f55a7
renmurakami <autoeyy@gmail.com>
2022-11-06 10:40:26 +0900

    style: form out style

--- src/components/form/searchForm.tsx
+++ src/components/form/searchForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import { FC, memo } from "react";
 import { useForm } from "react-hook-form";
 
@@ -13,21 +13,23 @@ const SearchForm: FC = () => {
   const onSubmit = useSearchForm();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} style={{ width: "80%" }}>
-      <Box display="flex" w="100%" p={8} shadow="lg" borderRadius={8}>
-        <TextInputMemo register={register} registerName="name_any" labelText="店舗名" isFirst />
-        <TextInputMemo register={register} registerName="keyword" labelText="キーワード" />
-        <RangeSelectMemo
-          register={register}
-          registerName="range"
-          labelText="検索範囲"
-          placeholder="現在地からの半径"
-        />
-        <Button type="submit" colorScheme="primary" borderLeftRadius={0}>
-          検索する
-        </Button>
-      </Box>
-    </form>
+    <Box w={{ base: "95%", lg: "80%" }} p={8} shadow="lg" borderRadius={8} bgColor="white">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex flexDir={{ base: "column", lg: "row" }} gap={{ base: 4, lg: 0 }}>
+          <TextInputMemo register={register} registerName="name_any" labelText="店舗名" isFirst />
+          <TextInputMemo register={register} registerName="keyword" labelText="キーワード" />
+          <RangeSelectMemo
+            register={register}
+            registerName="range"
+            labelText="検索範囲"
+            placeholder="現在地からの半径"
+          />
+          <Button type="submit" colorScheme="primary" borderLeftRadius={0}>
+            検索する
+          </Button>
+        </Flex>
+      </form>
+    </Box>
   );
 };
 
