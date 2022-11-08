5dcd44e33b0d569c6daa786db491315a1b072304
renmurakami <autoeyy@gmail.com>
2022-11-08 15:00:46 +0900

    refactor: fetch detail

--- src/pages/shop/[id].tsx
+++ src/pages/shop/[id].tsx
@@ -1,33 +1,11 @@
-import { Text } from "@chakra-ui/react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import useSWR from "swr";
-
-import { Shop } from "@/@types/api/shopType";
-import Footer from "@/components/footer/footer";
-import fetcher from "@/lib/fetcher";
+import Detail from "@/components/detail/detail";
+import Layout from "@/components/layout/layout";
 
 const ShopDetail = () => {
-  const router = useRouter();
-
-  const param = {
-    id: router.query.id,
-  };
-
-  const { data } = useSWR<Shop>(["/api/shop/detail", param], fetcher);
-
   return (
-    <div>
-      <Head>
-        <title>ちかれす - 店舗詳細</title>
-        <meta name="description" content="店舗詳細" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Text>{data ? data.name : "no"}</Text>
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <Detail />
+    </Layout>
   );
 };
 
