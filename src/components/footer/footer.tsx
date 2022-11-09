aef7102c320ff15f40b446897b9fc7b8f0c19a8d
renmurakami <autoeyy@gmail.com>
2022-11-09 22:24:14 +0900

    fix: footer link to /

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { FC } from "react";
 
 const Footer: FC = () => {
@@ -6,9 +7,11 @@ const Footer: FC = () => {
     <footer>
       <Box mt={12}>
         <Box w="full" py={12} bgColor="blackAlpha.200" px={12} display="flex" flexDir="column">
-          <Text as="small" fontSize="2xl" fontWeight="bold">
-            ちかれす
-          </Text>
+          <Link href="/">
+            <Text as="small" fontSize="2xl" fontWeight="bold">
+              ちかれす
+            </Text>
+          </Link>
           <Text as="small">本サービスはホットペッパー グルメサーチAPIを使用しています</Text>
         </Box>
       </Box>
